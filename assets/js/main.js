9424f146e250d9617b1697e3adde065697fca6c3
federossi93 <rossifederico0993@gmail.com>
2022-10-21 18:54:02 +0200

    add img

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,45 @@ costruiamo il container e inseriamo l'immagine grande in modo da poter stilare l
 Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
 Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.*/
 
+const images = [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];
+
+let caroselloEl = document.querySelector('.box');
+
+let Attiva = 0
+
+images.forEach((element,i) =>{
+    //console.log(element,i);
+    //console.log(element.image + " immagine della lista ");
+    //console.log(element.title + "titolo del film");
+    //console.log(element.text + " descrizione del film");
+    //creo elemento dom per appendere gli elementi
+    const slideMarkup = `<img class="${i === Attiva ? 'active' : ''}" src="./assets/${element.image}" alt="">`
+    caroselloEl.insertAdjacentHTML('beforeend', slideMarkup);
+    console.log(slideMarkup + "sono slide markup");
+})
+
 /*Milestone 2:
 Aggiungere il ciclo infinito del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso destra,
 la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso sinistra.*/
